0e4293de1f08f0ab6b4dfb0e3bf45ec0643aacc4
jerald-devOfficial <spaueOfficial@gmail.com>
2024-10-28 14:16:53 +0800

    feat: scaffold a react lottery application with web3 and lottery contract

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import lottery from "./lottery";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [manager, setManager] = useState("");
+
+  useEffect(() => {
+    async function fetchManager() {
+      const res = await lottery.methods.manager().call();
+      setManager(res);
+    }
+    fetchManager();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lottery Contract</h2>
+      <p>Manager: {manager}</p>
     </div>
   );
 }
