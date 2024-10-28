65791070ed5f9a49f05dd9ebb319e0a46848152a
jerald-devOfficial <spaueOfficial@gmail.com>
2024-10-28 14:25:16 +0800

    feat: add players and balance state in useReducer rather than useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import "./App.css";
 import lottery from "./lottery";
-import { useState, useEffect } from "react";
+import { useReducer, useEffect } from "react";
+import web3 from "./web3";
 
-function App() {
-  const [manager, setManager] = useState("");
+const initialState = {
+  manager: "",
+  players: [],
+  balance: 0,
+};
+
+const reducer = (state, action) => ({ ...state, ...action });
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    async function fetchManager() {
-      const res = await lottery.methods.manager().call();
-      setManager(res);
-    }
+    const fetchManager = async () => {
+      const manager = await lottery.methods.manager().call();
+      const players = await lottery.methods.getPlayers().call();
+      const balance = await web3.eth.getBalance(manager);
+
+      dispatch({ manager });
+      dispatch({ players });
+      dispatch({ balance });
+    };
     fetchManager();
   }, []);
 
   return (
     <div>
       <h2>Lottery Contract</h2>
-      <p>Manager: {manager}</p>
+      <p>Manager: {state.manager}</p>
+      <p>Players: {state.players}</p>
+      <p>Balance: {web3.utils.fromWei(state.balance, "ether")} ETH</p>
     </div>
   );
-}
+};
 
 export default App;
